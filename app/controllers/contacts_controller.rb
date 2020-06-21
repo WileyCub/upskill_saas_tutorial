ca5a3cd069b6ad26e8dd970a51f0e2191a308cea
Malakye Wiley Beagle <malakyewiley@icloud.com>
2020-06-21 16:53:08 +0000

    Added contact create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,19 @@
 class ContactsController < ApplicationController
    def new
+      @contact = Contact.new
    end 
-    
+   
+   def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+         redirect_to new_contact_path, notice: "Hey, thank you for your submission."
+      else
+         redirect_to new_contact_path, notice: "There was a problem with your submission."
+      end
+   end 
+   
+   private
+      def contact_params
+         params.require(:contact).permit(:name, :email, :comments)
+      end 
 end
